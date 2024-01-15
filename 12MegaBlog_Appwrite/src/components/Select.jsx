a49a9c82be6230c36cb0a9bf90c3b58684231cc2
CODingSAHIL <Your Name>
2024-01-15 22:14:02 +0530

    new comp added

---
+++ 12MegaBlog_Appwrite/src/components/Select.jsx
@@ -0,0 +1,33 @@
+import React, {useId} from 'react'
+
+function Select({
+    options,
+    label,
+    className = "",
+    ...props
+}, ref) {
+
+    //making id
+    const id = useId();
+
+
+  return (
+    <>
+        <div className='w-full'>
+            {label && <label htmlFor={id}></label>}
+            <select {...props} id={id} ref={ref} className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
+                {/* //options hme array se milega toh we have to loop through it  */}
+                {/* we will optionally loop on options if there is no option available then our app will crash  */}
+                {options?.map((option) => (
+                    <option key={option} value={option} >
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    </>
+  )
+}
+
+//2nd way of exporting forward ref in react .
+export default React.forwardRef(Select)
